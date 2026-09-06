4b77058b57971c4e952e76d2a1e6845715772150
NKBaa <362308040208@home.hpu.edu.cn>
2026-09-06 17:45:39 +0800

    release: prepare v1.0.7

--- Antigravity-Patcher.py
+++ Antigravity-Patcher.py
@@ -10,7 +10,7 @@ import time
 
 import sys
 
-PATCHER_VERSION = "v1.0.6"
+PATCHER_VERSION = "v1.0.7"
 UPDATE_API_URLS = [
     "https://ghfast.top/https://api.github.com/repos/NKBaa/Antigravity-zh-CN-Patcher/releases/latest",
     "https://mirror.ghproxy.com/https://api.github.com/repos/NKBaa/Antigravity-zh-CN-Patcher/releases/latest",
